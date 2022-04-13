cdb0632ff0f305430e7add5eca908e3ce8cd72fb
Sebastijan Zindl <sebastijanprime32@gmail.com>
2022-04-13 13:05:00 +0200

    initial commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Post;
+use App\Policies\PostPolicy;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        Post::class => PostPolicy::class,
+    ];
+    public function boot()
+    {
+        $this->registerPolicies();
+
+    }
+}
